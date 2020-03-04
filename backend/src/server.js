3f3479f133626db4ff88ea4bc3aa1e30e1fd4569
Francisco Junior <franciscojn49@gmail.com>
2020-03-04 14:42:01 -0300

    create backend (ok)

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-rndfj.mongodb.net/week9?retryWrites=true&w=majority', {
+    useCreateIndex: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(8080);
